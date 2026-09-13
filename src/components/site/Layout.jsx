6bd10c6a0133e9bb55d84653cde5460ac647acbd
Patrick Camacho <sentinelstechnologygroup@gmail.com>
2026-09-13 14:23:29 +0000

    External agent changes

--- src/components/site/Layout.jsx
+++ src/components/site/Layout.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import SEO from './SEO';
+import MarketingRuntime from './MarketingRuntime';
+import ConsentBanner from './ConsentBanner';
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-[#f4f1ea]">
+      <SEO />
+      <MarketingRuntime />
       <Header />
-      <main className="flex-1">
+      <main className="flex-1" id="main-content">
         <Outlet />
       </main>
       <Footer />
+      <ConsentBanner />
     </div>
   );
 }
